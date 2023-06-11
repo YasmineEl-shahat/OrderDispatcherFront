f0db36e47604798f594dd3317ca17c21a03cc299
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-06-11 09:15:52 +0300

    forget pass

--- pages/forget-password/index.js
+++ pages/forget-password/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import { useTranslation } from "../../util/useTranslation";
 import Image from "next/image";
-const Login = () => {
+
+const Forget = () => {
   const { t } = useTranslation();
   return (
     <>
       <Head>
         <title>
-          {t("login")} | {t("order-dispatcher")}
+          {t("forget")} | {t("order-dispatcher")}
         </title>
       </Head>
       <main className="login-container">
@@ -16,15 +17,12 @@ const Login = () => {
           <img src="/assets/logo.png" className="logo" alt="logo" />
           <figcaption>{t("order-dispatcher")}</figcaption>
         </figure>
-
         <div className="container" id="container">
           <div className="form-container sign-in-container">
             <form action="#">
-              <h1>{t("signin")}</h1>
+              <h1>{t("code")}</h1>
               <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <a href="#">{t("forget")}</a>
-              <button>{t("signin")}</button>
+              <button>{t("submit")}</button>
             </form>
           </div>
           <div className="overlay-container">
@@ -45,4 +43,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Forget;
