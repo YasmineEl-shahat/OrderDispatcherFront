af76e1d170f6ee743d72eddb1c0dbc35a8a7a2f1
Ayman Salah <ayman.1551.salah@gmail.com>
2023-06-12 10:31:55 +0300

    Order BoilerPlate

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -2,5 +2,6 @@ import { api_url } from "../../config/config";
 import { authorizedHttpJson } from "../../config/http";
 
 export async function getAllOrders() {
+  // Call axios Get >> return data 
   return await authorizedHttpJson.get(`${api_url}/orders`);
 }
--- pages/orders/index.js
+++ pages/orders/index.js
@@ -4,43 +4,56 @@ import Table from "../../src/sharedui/Table";
 import { getAllOrders } from "../api/orders";
 import { useState, useEffect } from "react";
 
+// Where to Call the orders endpoint
+// HACK -> In useEffect
+// ===============
+
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [orders, setOrders] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // const receivedOrders = getAllOrders();
-    // setOrders(receivedOrders);
+	useEffect(() => {
+		// const receivedOrders = getAllOrders();
+		// setOrders(receivedOrders);
 
-    setTimeout(() => {
-      setOrders([
-        { id: 1, name: "dddd" },
-        { id: 2, name: "dddd" },
-      ]);
-      setLoading(false);
-    }, 3000);
-  }, []);
+		// TODO >> Call the getall Orders Endpoint
+		getAllOrders()
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((error) => {
+				
+			});
 
-  const columnNames = ["id", "name"];
+		setTimeout(() => {
+			setOrders([
+				{ id: 1, name: "dddd" },
+				{ id: 2, name: "dddd" },
+			]);
+			setLoading(false);
+		}, 3000);
+	}, []);
 
-  return (
-    <main
-      className={
-        loading
-          ? `mainContainer d-flex justify-content-center align-items-center`
-          : `mainContainer`
-      }
-    >
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Table columnNames={columnNames} tableContent={orders} />
-      )}
-    </main>
-  );
+	const columnNames = ["id", "name"];
+
+	return (
+		<main
+			className={
+				loading
+					? `mainContainer d-flex justify-content-center align-items-center`
+					: `mainContainer`
+			}
+		>
+			{loading ? (
+				<Spinner />
+			) : (
+				<Table columnNames={columnNames} tableContent={orders} />
+			)}
+		</main>
+	);
 };
 Orders.getLayout = function getLayout(page) {
-  return <Layout title="Order">{page}</Layout>;
+	return <Layout title="Order">{page}</Layout>;
 };
 
 export default Orders;
