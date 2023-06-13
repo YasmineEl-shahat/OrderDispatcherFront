d4894829858845025419cac4c164182d24d5bc5f
Ayman Salah <ayman.1551.salah@gmail.com>
2023-06-13 12:53:33 +0300

    Polish

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -9,42 +9,42 @@ import { useState, useEffect } from "react";
 // ===============
 
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
-  const [columnNames, setColumnNames] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [orders, setOrders] = useState([]);
+	const [columnNames, setColumnNames] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      getAllOrders()
-        .then((res) => {
-          setOrders(res.data.data);
-          setColumnNames(Object.keys(res.data.data[0]));
-        })
-        .catch((error) => {
-          console.log(error.response.data.message);
-        });
-      setLoading(false);
-    }, 3000);
-  }, []);
+	useEffect(() => {
+		setTimeout(() => {
+			getAllOrders()
+				.then((res) => {
+					setOrders(res.data.data);
+					setColumnNames(Object.keys(res.data.data[0]));
+				})
+				.catch((error) => {
+					console.log(error.response.data.message);
+				});
+			setLoading(false);
+		}, 3000);
+	}, []);
 
-  return (
-    <main
-      className={
-        loading
-          ? `mainContainer d-flex justify-content-center align-items-center`
-          : `mainContainer`
-      }
-    >
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Table columnNames={columnNames} tableContent={orders} />
-      )}
-    </main>
-  );
+	return (
+		<main
+			className={
+				loading
+					? `mainContainer d-flex justify-content-center align-items-center`
+					: `mainContainer`
+			}
+		>
+			{loading ? (
+				<Spinner />
+			) : (
+				<Table columnNames={columnNames} tableContent={orders} />
+			)}
+		</main>
+	);
 };
 Orders.getLayout = function getLayout(page) {
-  return <Layout title="Order">{page}</Layout>;
+	return <Layout title="Order">{page}</Layout>;
 };
 
 export default Orders;
